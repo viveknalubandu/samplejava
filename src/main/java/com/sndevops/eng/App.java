66b457a5bf9920989c4085516178da0f700dea54
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-04-29 22:18:27 -0700

    clean up App.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,23 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // and I'm out...
-        // found a bug with that last commit
-        // fresh instance
-        // second commit
-        //r
-        // again
-        // tgo 
-        System.out.println( "Hello devops" );
+       
+        System.out.println( "Hello devops111" );
     }
 }
