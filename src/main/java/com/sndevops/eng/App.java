27aa2226f5ecfa4ad799e01fb8028e08ad588897
srilekha.sahoo <srilekha.sahoo@servicenow.com>
2019-04-29 22:16:52 -0700

    added app.java

---
+++ src/main/java/com/sndevops/eng/App.java
@@ -0,0 +1,30 @@
+package com.sndevops.eng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // This is a Test
+        // and I'm out...
+        // found a bug with that last commit
+        // fresh instance
+        // second commit
+        //r
+        // again
+        // tgo 
+        System.out.println( "Hello devops" );
+    }
+}
---
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -0,0 +1,15 @@
+package com.sndevops.eng;
+import org.junit.Assert;
+import org.junit.Test; 
+
+public class AppTest {
+// rrt   
+    // Test
+    private String INPUT = "13411";
+    @Test 
+    public void testLength() { 
+        Assert.assertEquals(INPUT,"13411");  
+    }
+}
+ 
+
