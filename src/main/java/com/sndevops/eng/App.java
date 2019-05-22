094973892277956f6e1d81d768d838b9461e14c8
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-05-22 11:18:51 -0700

    updated commit

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops123===");
+        System.out.println( "Hello devops123");
     }
 }
