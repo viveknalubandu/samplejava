ecc8eb5088617eeb8f36261245592d1879043805
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-05-30 16:14:59 -0700

    updated app.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops123345werd");
+        System.out.println( "Hello devops123345werddddd");
     }
 }
