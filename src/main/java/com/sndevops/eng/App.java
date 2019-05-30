233075b79d4fd0f7d38d05ca8fff89ace5f872b2
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-05-30 12:04:27 -0700

    updated App class in new branch

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops123345");
+        System.out.println( "Hello devops1233sdgtf");
     }
 }
