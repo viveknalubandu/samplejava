9575a716f18252441db42afbc8d2f147ed82c2ef
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-02 16:35:33 -0700

    check CG success rate

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops123345werddddeeessfddgffdst");
+        System.out.println( "Hello devops123345werddddeeessfddgerst");
     }
 }
