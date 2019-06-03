76639ceef7813c30f30445bca82472ec96474522
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-03 11:29:28 -0700

    Checking CG-June03

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops123345werddddeeessfddsfetry");
+        System.out.println( "Hello devops123345werddddeeessfddsferstdg");
     }
 }
