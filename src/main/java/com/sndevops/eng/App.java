605d1df017323c5dd29c700b7622c5910d212566
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-14 12:01:16 -0700

    com21:update app.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops12esdertwwert");
+        System.out.println( "Hello devops12esdertwertsew");
     }
 }
