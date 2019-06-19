91ef29dd075caa8d00409eefd2d0ebda0473c300
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-19 10:32:34 -0700

    Com19: update app.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops world1245sdf13sdfwre wtyd");
+        System.out.println( "Hello devops world1245sdf13sdfwre ertsdwe");
     }
 }
