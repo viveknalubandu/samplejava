cde20a12a88dd34892e57e0ed7489701f02ab7bd
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-20 13:10:51 -0700

    change

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops world1245sdf13sdf2345");
+        System.out.println( "Hello devops world1245sdf13sdtyr");
     }
 }
