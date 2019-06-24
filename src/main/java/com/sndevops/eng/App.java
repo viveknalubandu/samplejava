964b8f1a3cf0ef7c4d6e2f3132c56ccad3284388
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-24 15:06:49 -0700

    com:update123

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops world12sfdget");
+        System.out.println( "Hello devops world12sfdter");
     }
 }
