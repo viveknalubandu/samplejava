0242c2877b5f3df238d3f02e10c6193efe648076
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-06-26 18:33:13 -0700

    COMMIT234

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops worlds334528976");
+        System.out.println( "Hello devops worlds33452");
     }
 }
