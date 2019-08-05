765f94ac07707d4801956281d9d7f5d087e78597
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-08-05 09:49:03 -0700

    update app.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops world3werewert");
+        System.out.println( "Hello devops world3were234wer");
     }
 }
