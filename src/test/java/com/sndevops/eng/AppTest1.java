abdfdeced77e5d04538750cd8bac8ff34dde2a76
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-10-04 11:47:47 -0700

    Update AppTest1.java

--- src/test/java/com/sndevops/eng/AppTest1.java
+++ src/test/java/com/sndevops/eng/AppTest1.java
@@ -1,1 +1,13 @@
-xxxx
+package com.sndevops.eng;
+import org.junit.Assert;
+import org.junit.Test; 
+
+public class AppTest1 {
+// rrt   13411
+    // Test
+    private String INPUT = "13412";
+    @Test 
+    public void testLength() { 
+        Assert.assertEquals(INPUT,"13412");  
+    }
+}
