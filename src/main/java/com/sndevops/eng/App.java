59469b43e8e67117605a3b2731188522d3aa67cd
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-10-11 17:47:43 -0700

    Update App.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops worldw ---upgrade 12333wwww2");
+        System.out.println( "Hello devops worldw ---upgrade 12333657");
     }
 }
