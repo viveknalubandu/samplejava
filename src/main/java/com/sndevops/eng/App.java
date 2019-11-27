b73090db72400bd34f629acb0c96825c247cf58d
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-11-27 12:02:40 -0800

    Com:Update for devops4

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
        
-        System.out.println( "Hello devops worldw ---upgrade 2145");
+        System.out.println( "Hello devops worldw ---upgrade 21354");
     }
 }
